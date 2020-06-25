b71ec2b69717de0ed45d6d0fde098a58caf412af
masty123 <pwnimatorz@hotmail.com>
2020-06-25 14:05:49 +0700

    first commit

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward,out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+
+    }
+}
+
+
