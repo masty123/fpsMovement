7a9ca8b93ad01b00dbefe93454da638c89ea8c5e
masty123 <pwnimatorz@hotmail.com>
2020-07-03 09:42:42 +0700

    shooting

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,8 +4,14 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float impactForce = 300f;
+    public float fireRate = 15f;
 
     public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,18 +21,33 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
     void Shoot()
     {
+        muzzleFlash.Play();
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward,out hit, range))
         {
             Debug.Log(hit.transform.name);
+
+            TargetObj target = hit.transform.GetComponent<TargetObj>();
+            if(target != null)
+            {
+                target.takeDmg(damage);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
         }
 
     }
---
+++ Assets/Scripts/TargetObj.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetObj : MonoBehaviour
+{
+    public float health = 50f;
+    public void takeDmg(float amount)
+    {
+        health -= amount;
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
